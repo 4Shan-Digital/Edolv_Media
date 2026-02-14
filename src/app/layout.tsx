9291742a4515633739d5a2e63055c31cc18329bb
Himanshusharma2046 <himanshusharma4041@gmail.com>
2026-02-15 01:13:13 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,9 +77,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="icon" href="/images/Edolv png.png" sizes="any" />
-        <link rel="icon" href="/images/Edolv png.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/images/Edolv png.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         {/* Structured Data for SEO */}
         <script
