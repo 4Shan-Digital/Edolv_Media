51daf5bc1b938f91a744044b4cbb5b8618940d52
Himanshusharma2046 <himanshusharma4041@gmail.com>
2026-02-18 11:46:17 +0530

    Fix: Allow sitemap.xml in robots.txt and improve SEO metadata

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,18 +2,19 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/api/',
-        '/admin/',
-        '/*.ico',
-        '/*.json',
-        '/*.xml',
-        '/_next/',
-      ],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/sitemap.xml'],
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/*.ico',
+          '/*.json',
+          '/_next/',
+        ],
+      },
+    ],
     sitemap: 'https://edolv.com/sitemap.xml',
   };
 }
